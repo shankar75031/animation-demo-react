f92e21df5cb86cd45cbd5ad415c18c5eef0bad71
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-20 02:12:50 +0530

    changed transition component to csstransition

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -9,38 +9,29 @@ const animationTiming = {
 };
 
 const modal = (props) => {
+  // const cssClasses = [
+  //   "Modal",
+  //   state === "entering"
+  //     ? "ModalOpen"
+  //     : state === "exiting"
+  //     ? "ModalClose"
+  //     : null,
+  // ];
   return (
-    <Transition
+    <CSSTransition
       in={props.show}
       timeout={animationTiming}
       mountOnEnter
       unmountOnExit
-      onEnter={() => console.log("onEnter")}
-      onEntering={() => console.log("onEntering")}
-      onEntered={() => console.log("onEntered")}
-      onExit={() => console.log("onExit")}
-      onExiting={() => console.log("onExiting")}
-      onExited={() => console.log("onExited")}
+      classNames={"fade-slide"}
     >
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClose"
-            : null,
-        ];
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
