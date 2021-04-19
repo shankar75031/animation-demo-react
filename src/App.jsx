8fabb1516ce8a46fb50627512297307491e9dd38
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-20 01:47:39 +0530

    added transition component from react transiton group

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import { Transition } from "react-transition-group";
 
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -22,13 +24,42 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button
+          className="Button"
+          onClick={() => {
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }));
+          }}
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => {
+            return (
+              <div
+                style={{
+                  backgroundColor: "red",
+                  width: 100,
+                  height: 100,
+                  transition: "opacity 1s ease-out",
+                  opacity: state === "exiting" ? 0 : 1,
+                  margin: "auto",
+                }}
+              />
+            );
+          }}
+        </Transition>
         {this.state.modalIsOpen ? (
           <Modal closed={this.closeModal} show={this.state.modalIsOpen} />
         ) : null}
         {this.state.modalIsOpen ? (
           <Backdrop show={this.state.modalIsOpen} />
         ) : null}
-
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
